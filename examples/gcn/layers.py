the model is incorrect (loss, softmax-cross-entropy, etc) but compiles

examples/gcn/layers.py
@@ -1,8 +1,8 @@
-from inits import *
 import tensorflow as tf
 
-flags = tf.app.flags
-FLAGS = flags.FLAGS
+from inits import *
+from tensorflow.keras.layers import Layer, Dense
+from tensorflow.keras.initializers import glorot_uniform
 
 # global unique layer ID dictionary for layer name assignment
 _LAYER_UIDS = {}
@@ -21,69 +21,23 @@ def get_layer_uid(layer_name=""):
 def sparse_dropout(x, keep_prob, noise_shape):
     """Dropout for sparse tensors."""
     random_tensor = keep_prob
-    random_tensor += tf.random_uniform(noise_shape)
+    random_tensor += tf.random.uniform(noise_shape)
     dropout_mask = tf.cast(tf.floor(random_tensor), dtype=tf.bool)
-    pre_out = tf.sparse_retain(x, dropout_mask)
+    pre_out = tf.sparse.retain(x, dropout_mask)
     return pre_out * (1.0 / keep_prob)
 
 
-def dot(x, y, sparse=False):
+def dot(x, y):
     """Wrapper for tf.matmul (sparse vs dense)."""
-    if sparse:
-        res = tf.sparse_tensor_dense_matmul(x, y)
+    if isinstance(x, tf.SparseTensor):
+        res = tf.sparse.sparse_dense_matmul(x, y)
     else:
         res = tf.matmul(x, y)
     return res
 
 
-class Layer(object):
-    """Base layer class. Defines basic API for all layer objects.
-    Implementation inspired by keras (http://keras.io).
-
-    # Properties
-        name: String, defines the variable scope of the layer.
-        logging: Boolean, switches Tensorflow histogram logging on/off
-
-    # Methods
-        _call(inputs): Defines computation graph of layer
-            (i.e. takes input, returns output)
-        __call__(inputs): Wrapper for _call()
-        _log_vars(): Log all variables
-    """
-
-    def __init__(self, **kwargs):
-        allowed_kwargs = {"name", "logging"}
-        for kwarg in kwargs.keys():
-            assert kwarg in allowed_kwargs, "Invalid keyword argument: " + kwarg
-        name = kwargs.get("name")
-        if not name:
-            layer = self.__class__.__name__.lower()
-            name = layer + "_" + str(get_layer_uid(layer))
-        self.name = name
-        self.vars = {}
-        logging = kwargs.get("logging", False)
-        self.logging = logging
-        self.sparse_inputs = False
-
-    def _call(self, inputs):
-        return inputs
-
-    def __call__(self, inputs):
-        with tf.name_scope(self.name):
-            if self.logging and not self.sparse_inputs:
-                tf.summary.histogram(self.name + "/inputs", inputs)
-            outputs = self._call(inputs)
-            if self.logging:
-                tf.summary.histogram(self.name + "/outputs", outputs)
-            return outputs
-
-    def _log_vars(self):
-        for var in self.vars:
-            tf.summary.histogram(self.name + "/vars/" + var, self.vars[var])
-
-
-class Dense(Layer):
-    """Dense layer."""
+class GraphConvolution(Layer):
+    """Graph convolution layer."""
 
     def __init__(
         self,
@@ -112,65 +66,7 @@ class Dense(Layer):
         # helper variable for sparse dropout
         self.num_features_nonzero = placeholders["num_features_nonzero"]
 
-        with tf.variable_scope(self.name + "_vars"):
-            self.vars["weights"] = glorot([input_dim, output_dim], name="weights")
-            if self.bias:
-                self.vars["bias"] = zeros([output_dim], name="bias")
-
-        if self.logging:
-            self._log_vars()
-
-    def _call(self, inputs):
-        x = inputs
-
-        # dropout
-        if self.sparse_inputs:
-            x = sparse_dropout(x, 1 - self.dropout, self.num_features_nonzero)
-        else:
-            x = tf.nn.dropout(x, 1 - self.dropout)
-
-        # transform
-        output = dot(x, self.vars["weights"], sparse=self.sparse_inputs)
-
-        # bias
-        if self.bias:
-            output += self.vars["bias"]
-
-        return self.act(output)
-
-
-class GraphConvolution(Layer):
-    """Graph convolution layer."""
-
-    def __init__(
-        self,
-        input_dim,
-        output_dim,
-        placeholders,
-        dropout=0.0,
-        sparse_inputs=False,
-        act=tf.nn.relu,
-        bias=False,
-        featureless=False,
-        **kwargs
-    ):
-        super(GraphConvolution, self).__init__(**kwargs)
-
-        if dropout:
-            self.dropout = placeholders["dropout"]
-        else:
-            self.dropout = 0.0
-
-        self.act = act
-        self.support = placeholders["support"]
-        self.sparse_inputs = sparse_inputs
-        self.featureless = featureless
-        self.bias = bias
-
-        # helper variable for sparse dropout
-        self.num_features_nonzero = placeholders["num_features_nonzero"]
-
-        with tf.variable_scope(self.name + "_vars"):
+        with tf.compat.v1.variable_scope(self.name + "_vars"):
             for i in range(len(self.support)):
                 self.vars["weights_" + str(i)] = glorot(
                     [input_dim, output_dim], name="weights_" + str(i)
@@ -183,12 +79,11 @@ class GraphConvolution(Layer):
 
     def _call(self, inputs):
         x = inputs
-
         # dropout
         if self.sparse_inputs:
             x = sparse_dropout(x, 1 - self.dropout, self.num_features_nonzero)
         else:
-            x = tf.nn.dropout(x, 1 - self.dropout)
+            x = tf.nn.dropout(x, 1 - (1 - self.dropout))
 
         # convolve
         supports = list()
@@ -199,6 +94,7 @@ class GraphConvolution(Layer):
                 )
             else:
                 pre_sup = self.vars["weights_" + str(i)]
+
             support = dot(self.support[i], pre_sup, sparse=True)
             supports.append(support)
         output = tf.add_n(supports)
@@ -208,3 +104,97 @@ class GraphConvolution(Layer):
             output += self.vars["bias"]
 
         return self.act(output)
+
+
+class GraphConvolutionKeras(Layer):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        supports,
+        num_features_nonzero,
+        dropout=0.0,
+        sparse_inputs=False,
+        activation=tf.nn.relu,
+        use_bias=False,
+        featureless=False,
+        **kwargs
+    ):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.supports = supports
+        self.num_features_nonzero = num_features_nonzero
+        self.dropout = dropout
+        self.activation = activation
+        self.sparse_inputs = sparse_inputs
+        self.featureless = featureless
+        self.use_bias = use_bias
+
+        # helper variable for sparse dropout
+        # self.num_features_nonzero = placeholders["num_features_nonzero"]
+
+        super(GraphConvolutionKeras, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        # with tf.compat.v1.variable_scope(self.name + "_vars"):
+        #     for i in range(len(self.support)):
+        #         self.vars["weights_" + str(i)] = glorot(
+        #             [input_dim, output_dim], name="weights_" + str(i)
+        #         )
+        #     if self.bias:
+        #         self.vars["bias"] = zeros([output_dim], name="bias")
+
+        # Create a trainable weight variable for this layer for each support
+        self.weights_per_support = [
+            self.add_weight(
+                name="weight_support_{}".format(i),
+                shape=(self.input_dim, self.output_dim),
+                initializer=glorot_uniform(seed=None),
+                trainable=True,
+            )
+            for i in range(len(self.supports))
+        ]
+
+        if self.use_bias:
+            self.bias = self.add_weight(
+                name="bias", shape=output_dim, initializer="zeros", trainable=False
+            )
+        # self.kernel = self.add_weight(
+        #     name="kernel",
+        #     shape=(len(self.support), self.input_dim, self.output_dim),
+        #     initializer=keras.initializers.glorot_uniform(seed=None),
+        #     trainable=True,
+        # )
+
+        # Create trainable bias variable for this layer.
+        super(GraphConvolutionKeras, self).build(
+            input_shape
+        )  # Be sure to call this at the end
+
+    def call(self, x):
+
+        if isinstance(x, tf.SparseTensor):
+            # Input is sparse, apply sparse dropout
+            x = sparse_dropout(x, 1 - self.dropout, self.num_features_nonzero)
+        else:
+            x = tf.nn.dropout(x, 1 - (1 - self.dropout))
+
+        # convolve
+        supports = list()
+        for i in range(len(self.supports)):
+            weight_name = "weight_support_{}".format(i)
+            if not self.featureless:
+                pre_sup = dot(x, self.weights_per_support[i])
+            else:
+                pre_sup = self.weights_per_support[i]
+
+            support = dot(self.supports[i], pre_sup)
+            supports.append(support)
+        output = tf.add_n(supports)
+
+        # bias
+        if self.use_bias:
+            output += self.bias
+
+        return self.activation(output)

examples/gcn/train.py
@@ -1,49 +1,43 @@
-from __future__ import division
-from __future__ import print_function
+from __future__ import division, print_function
 
 import time
 import tensorflow as tf
 
 from utils import *
 from evaluate import evaluate
-from models import GCN, MLP
+from models import GCN, GCN_Keras
+
+from absl import app
+from absl import flags
+
+
+# def train_step(source_seq, target_seq_in, target_seq_out, en_initial_states):
+#     with tf.GradientTape() as tape:
+#         en_outputs = encoder(source_seq, en_initial_states)
+#         en_states = en_outputs[1:]
+#         de_states = en_states
+
+#         de_outputs = decoder(target_seq_in, de_states)
+#         logits = de_outputs[0]
+#         loss = loss_func(target_seq_out, logits)
+
+#     variables = encoder.trainable_variables + decoder.trainable_variables
+#     gradients = tape.gradient(loss, variables)
+#     optimizer.apply_gradients(zip(gradients, variables))
+
+#     return loss
+
+# def training_step(model, inputs):
+#     with tf.GradientTape() as tape:
 
 
 def train(config):
 
     # Set random seed
     np.random.seed(config.seed)
-    tf.set_random_seed(config.seed)
+    tf.compat.v1.set_random_seed(config.seed)
 
-    # Settings pulled from configuration file
-    flags = tf.app.flags
-    FLAGS = flags.FLAGS
-    flags.DEFINE_string(
-        "dataset", config.dataset, "Dataset string."
-    )  # "cora", "citeseer", "pubmed"
-    flags.DEFINE_string(
-        "model", config.model, "Model string."
-    )  # "gcn", "gcn_cheby", "dense"
-    flags.DEFINE_float("learning_rate", config.learning_rate, "Initial learning rate.")
-    flags.DEFINE_integer("epochs", config.epochs, "Number of epochs to train.")
-    flags.DEFINE_integer(
-        "hidden1", config.hidden1, "Number of units in hidden layer 1."
-    )
-    flags.DEFINE_float(
-        "dropout", config.dropout, "Dropout rate (1 - keep probability)."
-    )
-    flags.DEFINE_float(
-        "weight_decay", config.weight_decay, "Weight for L2 loss on embedding matrix."
-    )
-    flags.DEFINE_integer(
-        "early_stopping",
-        config.early_stopping,
-        "Tolerance for early stopping (# of epochs).",
-    )
-    flags.DEFINE_integer(
-        "max_degree", config.max_degree, "Maximum Chebyshev polynomial degree."
-    )
-
+    # Load dataset
     adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(
         config.dataset
     )
@@ -52,91 +46,97 @@ def train(config):
     features = preprocess_features(features)
 
     if config.model == "gcn":
+        # Support is sparse representation of normalized adjacency matrix (coords, values)
+        # TODO: consider renaming "support"
         support = [preprocess_adj(adj)]
         num_supports = 1
-        model_constructor = GCN
+        model_constructor = GCN_Keras
     elif config.model == "gcn_cheby":
         support = chebyshev_polynomials(adj, config.max_degree)
         num_supports = 1 + config.max_degree
         model_constructor = GCN
     elif config.model == "dense":
+        # TODO: should be able to use Keras OOTB
         support = [preprocess_adj(adj)]  # Not used
         num_supports = 1
         model_constructor = MLP
     else:
         raise ValueError("Invalid argument for model: {0}".format(config.model))
 
+    model = model_constructor(
+        input_dim=features.shape[1],
+        output_dim=y_train.shape[1],
+        supports=support,
+        num_features_nonzero=features.values.shape,
+    )
+
     # Define placeholders
-    placeholders = {
-        "support": [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-        "features": tf.sparse_placeholder(
-            tf.float32, shape=tf.constant(features[2], dtype=tf.int64)
-        ),
-        "labels": tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-        "labels_mask": tf.placeholder(tf.int32),
-        "dropout": tf.placeholder_with_default(0.0, shape=()),
-        "num_features_nonzero": tf.placeholder(
-            tf.int32
-        ),  # helper variable for sparse dropout
-    }
+    # placeholders = {
+    #     "support": [
+    #         tf.compat.v1.sparse_placeholder(tf.float32) for _ in range(num_supports)
+    #     ],
+    #     "features": tf.compat.v1.sparse_placeholder(
+    #         tf.float32, shape=tf.constant(features[2], dtype=tf.int64)
+    #     ),
+    #     "labels": tf.compat.v1.placeholder(tf.float32, shape=(None, y_train.shape[1])),
+    #     "labels_mask": tf.compat.v1.placeholder(tf.int32),
+    #     "dropout": tf.compat.v1.placeholder_with_default(0.0, shape=()),
+    #     "num_features_nonzero": tf.compat.v1.placeholder(
+    #         tf.int32
+    #     ),  # helper variable for sparse dropout
+    # }
 
     # Create model
-    model = model_constructor(placeholders, input_dim=features[2][1], logging=True)
+    # model = model_constructor(placeholders, input_dim=features[2][1], logging=True)
 
-    # Initialize session
-    sess = tf.Session()
+    # cost_val = []
 
-    # Init variables
-    sess.run(tf.global_variables_initializer())
+    # # Train model
+    # for epoch in range(config.epochs):
 
-    cost_val = []
+    #     t = time.time()
 
-    # Train model
-    for epoch in range(config.epochs):
+    #     # Construct feed dictionary
+    #     feed_dict = construct_feed_dict(
+    #         features, support, y_train, train_mask, placeholders
+    #     )
 
-        t = time.time()
+    #     feed_dict.update({placeholders["dropout"]: config.dropout})
 
-        # Construct feed dictionary
-        feed_dict = construct_feed_dict(
-            features, support, y_train, train_mask, placeholders
-        )
+    #     # Training step
+    #     outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
 
-        feed_dict.update({placeholders["dropout"]: config.dropout})
+    #     # Validation
+    #     val_feed_dict = construct_feed_dict(
+    #         features, support, y_val, val_mask, placeholders
+    #     )
+    #     cost, acc, duration = evaluate(sess, model, val_feed_dict)
+    #     cost_val.append(cost)
 
-        # Training step
-        outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
+    #     # Print results
+    #     print(
+    #         "Epoch: {:04d}, train_loss= {:.5f}, train_acc={:.5f}, val_loss={:.5f}, val_acc={:.5f}, time={:.5f}".format(
+    #             (epoch + 1), outs[1], outs[2], cost, acc, (time.time() - t)
+    #         )
+    #     )
 
-        # Validation
-        val_feed_dict = construct_feed_dict(
-            features, support, y_val, val_mask, placeholders
-        )
-        cost, acc, duration = evaluate(sess, model, val_feed_dict)
-        cost_val.append(cost)
+    #     # Early stopping
+    #     if epoch > config.early_stopping and cost_val[-1] > np.mean(
+    #         cost_val[-(config.early_stopping + 1) : -1]
+    #     ):
+    #         print("Early stopping...")
+    #         break
 
-        # Print results
-        print(
-            "Epoch: {:04d}, train_loss= {:.5f}, train_acc={:.5f}, val_loss={:.5f}, val_acc={:.5f}, time={:.5f}".format(
-                (epoch + 1), outs[1], outs[2], cost, acc, (time.time() - t)
-            )
-        )
+    # print("Optimization Finished!")
 
-        # Early stopping
-        if epoch > config.early_stopping and cost_val[-1] > np.mean(
-            cost_val[-(config.early_stopping + 1) : -1]
-        ):
-            print("Early stopping...")
-            break
+    # # Evaluation on test set
+    # test_feed_dict = construct_feed_dict(
+    #     features, support, y_test, test_mask, placeholders
+    # )
+    # test_cost, test_acc, test_duration = evaluate(sess, model, test_feed_dict)
 
-    print("Optimization Finished!")
-
-    # Evaluation on test set
-    test_feed_dict = construct_feed_dict(
-        features, support, y_test, test_mask, placeholders
-    )
-    test_cost, test_acc, test_duration = evaluate(sess, model, test_feed_dict)
-
-    print(
-        "Test set results: cost= {:.5f}, accuracy={:.5f}, time={:.5f}".format(
-            test_cost, test_acc, test_duration
-        )
-    )
+    # print(
+    #     "Test set results: cost= {:.5f}, accuracy={:.5f}, time={:.5f}".format(
+    #         test_cost, test_acc, test_duration
+    #     )
+    # )

examples/gcn/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle as pkl
 import networkx as nx
 import scipy.sparse as sp
+import tensorflow as tf
 
 from dotmap import DotMap
 from tensorflow.python.lib.io import file_io
@@ -158,24 +159,24 @@ def load_data(dataset_str):
     return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
 
 
-def sparse_to_tuple(sparse_mx):
+def sparse_matrix_to_sparse_tensor(sparse_mx):
     """Convert sparse matrix to tuple representation."""
 
-    def to_tuple(mx):
+    def to_sparse_tensor(mx):
         if not sp.isspmatrix_coo(mx):
             mx = mx.tocoo()
         coords = np.vstack((mx.row, mx.col)).transpose()
         values = mx.data
         shape = mx.shape
-        return coords, values, shape
+        return tf.SparseTensor(coords, values, shape)
 
     if isinstance(sparse_mx, list):
         for i in range(len(sparse_mx)):
-            sparse_mx[i] = to_tuple(sparse_mx[i])
+            sparse_mx[i] = to_sparse_tensor(sparse_mx[i])
     else:
-        sparse_mx = to_tuple(sparse_mx)
+        sparse_mx = to_sparse_tensor(sparse_mx)
 
-    return sparse_mx
+    return tf.cast(sparse_mx, tf.float32)
 
 
 def preprocess_features(features):
@@ -185,7 +186,7 @@ def preprocess_features(features):
     r_inv[np.isinf(r_inv)] = 0.0
     r_mat_inv = sp.diags(r_inv)
     features = r_mat_inv.dot(features)
-    return sparse_to_tuple(features)
+    return sparse_matrix_to_sparse_tensor(features)
 
 
 def normalize_adj(adj):
@@ -201,7 +202,7 @@ def normalize_adj(adj):
 def preprocess_adj(adj):
     """Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
     adj_normalized = normalize_adj(adj + sp.eye(adj.shape[0]))
-    return sparse_to_tuple(adj_normalized)
+    return sparse_matrix_to_sparse_tensor(adj_normalized)
 
 
 def construct_feed_dict(features, support, labels, labels_mask, placeholders):
@@ -237,4 +238,4 @@ def chebyshev_polynomials(adj, k):
     for i in range(2, k + 1):
         t_k.append(chebyshev_recurrence(t_k[-1], t_k[-2], scaled_laplacian))
 
-    return sparse_to_tuple(t_k)
+    return sparse_matrix_to_sparse_tensor(t_k)