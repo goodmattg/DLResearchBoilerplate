closer to Kipf. Erroneous last Dense layer now softmax activation

examples/gcn/models.py
@@ -2,7 +2,7 @@ from layers import *
 from metrics import *
 
 from tensorflow.keras import optimizers, regularizers, Sequential
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Activation
 from tensorflow.keras.models import Model
 
 
@@ -67,13 +67,8 @@ class GCN(Model):
 
 
 # Define custom loss
-def masked_loss(layer):
-
-    # Create a loss function that adds the MSE loss to the mean of all squared activations of a specific layer
+def masked_loss(self):
     def _loss(y_true, y_pred):
-
-        print("Y_TRUE")
-        print(y_true.shape)
 
         loss = 0
 
@@ -89,21 +84,25 @@ def masked_loss(layer):
     return _loss
 
 
+def _masked_accuracy(y_true, y_pred):
+    return masked_accuracy(y_true[:, :-1], y_pred, y_true[:, -1])
+
+
 def GCN_Keras(input_dim, output_dim, supports, num_features_nonzero, config, **kwargs):
 
     model = Sequential()
 
     model.add(Input(batch_shape=input_dim, sparse=True))
 
-    layer1 = GraphConvolutionKeras(
-        output_dim=config.hidden1,
-        supports=supports,
-        num_features_nonzero=num_features_nonzero,
-        activation=tf.nn.relu,
-        dropout=config.dropout,
+    model.add(
+        GraphConvolutionKeras(
+            output_dim=config.hidden1,
+            supports=supports,
+            num_features_nonzero=num_features_nonzero,
+            activation=tf.nn.relu,
+            dropout=config.dropout,
+        )
     )
-
-    model.add(layer1)
 
     # TODO: MISSING REGULARIZATION OF FIRST LAYER
     # model.add(regularizers.l2(config.weight_decay))
@@ -115,16 +114,16 @@ def GCN_Keras(input_dim, output_dim, supports, num_features_nonzero, config, **k
             output_dim=output_dim,
             supports=supports,
             num_features_nonzero=num_features_nonzero,
-            activation=tf.identity,
+            activation=tf.nn.softmax,
             dropout=config.dropout,
         )
     )
 
-    model.add(Dense(output_dim, activation="softmax"))
+    # model.add(Dense(output_dim, activation="softmax"))
 
     model.compile(
-        loss=masked_loss(layer1),
-        metrics=["accuracy"],
+        loss=masked_loss(model),
+        metrics=[_masked_accuracy],
         optimizer=optimizers.Adam(lr=config.learning_rate),
     )
 

examples/gcn/train.py
@@ -7,29 +7,6 @@ from utils import *
 from evaluate import evaluate
 from models import GCN, GCN_Keras
 
-from absl import app
-from absl import flags
-
-
-# def train_step(source_seq, target_seq_in, target_seq_out, en_initial_states):
-#     with tf.GradientTape() as tape:
-#         en_outputs = encoder(source_seq, en_initial_states)
-#         en_states = en_outputs[1:]
-#         de_states = en_states
-
-#         de_outputs = decoder(target_seq_in, de_states)
-#         logits = de_outputs[0]
-#         loss = loss_func(target_seq_out, logits)
-
-#     variables = encoder.trainable_variables + decoder.trainable_variables
-#     gradients = tape.gradient(loss, variables)
-#     optimizer.apply_gradients(zip(gradients, variables))
-
-#     return loss
-
-# def training_step(model, inputs):
-#     with tf.GradientTape() as tape:
-
 
 def train(config):
 
@@ -42,8 +19,7 @@ def train(config):
         config.dataset
     )
 
-    # Some preprocessing
-    features = preprocess_features(features)
+    # Feature preprocessing
     norm_feat = preprocess_features(features)
     sparse_norm_feat = sparse_matrix_to_sparse_tensor(norm_feat)
 
@@ -77,8 +53,6 @@ def train(config):
         config=config.model,
     )
 
-    #         tf.boolean_mask(y_val, val_mask),
-
     print(model.metrics_names)
     for epoch in range(config.training.epochs):
         t = time.time()
@@ -97,22 +71,6 @@ def train(config):
         )
 
     # TODO: APPLY MASKING BEFOREHAND before trying to fit everything (i.e. train)
-
-    # Define placeholders
-    # placeholders = {
-    #     "support": [
-    #         tf.compat.v1.sparse_placeholder(tf.float32) for _ in range(num_supports)
-    #     ],
-    #     "features": tf.compat.v1.sparse_placeholder(
-    #         tf.float32, shape=tf.constant(features[2], dtype=tf.int64)
-    #     ),
-    #     "labels": tf.compat.v1.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-    #     "labels_mask": tf.compat.v1.placeholder(tf.int32),
-    #     "dropout": tf.compat.v1.placeholder_with_default(0.0, shape=()),
-    #     "num_features_nonzero": tf.compat.v1.placeholder(
-    #         tf.int32
-    #     ),  # helper variable for sparse dropout
-    # }
 
     # Create model
     # model = model_constructor(placeholders, input_dim=features[2][1], logging=True)